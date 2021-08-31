7a769d2aa803d8e19744b3b0863c105c2a7b8a13
Men Hao <171110115@stu.hit.edu.cn>
2021-08-31 22:14:00 +0800

    按degree完成

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ dst = './dst'
 
 file_map = {
     'Youtube-edge.csv': 'Youtube-node1.csv',
-    'Wiki-Vote-edge.csv': 'wiki-node1.csv',
-    'Facebook-edge.csv': 'Facebook-node.csv',
+    # 'Wiki-Vote-edge.csv': 'wiki-node1.csv',
+    # 'Facebook-edge.csv': 'Facebook-node.csv',
 }
 
 
@@ -31,7 +31,6 @@ def load_graph(file_name):
     edges = load_edges(file)
     G = nx.Graph()
     G.add_edges_from(edges)
-    init_feature(G, file_name)
     return G
 
 
@@ -43,61 +42,69 @@ def load_nodes(file_name):
     return nodes
 
 
-def init_feature(G, file_name):
+def init_feature(G, type='degree'):
     """为图的节点设置属性"""
-    node_file = file_map[file_name]
-    nodes = load_nodes(node_file)
-    for info in nodes:
-        id = info.pop('Id')
-        G.nodes[id].update(info)
+
+    data = {}
+    if type == 'degree':
+        for k, v in G.degree():
+            data[k] = v
+    if type == 'closnesscentrality':
+        data = nx.closeness_centrality(G)
+    if type == 'eigencentrality':
+        data = nx.eigenvector_centrality(G)
+    if type == 'betweenesscentrality':
+        data = nx.betweenness_centrality(G)
+    for k, v in data.items():
+        G.nodes[k][type] = v
 
 
 def remove(G, type='degree'):
     """执行删除操作，并记录值"""
     node_num = len(G.nodes)  # 节点总数
-    label = '{}({})'.format(node_num,types[type])
+    label = '{}({})'.format(node_num, types[type])
     x = [int(0.01 * x * node_num) for x in range(1, 101)]  # 每次移除总数
+
+    print("all num is %d" % node_num)
     s_q = Queue()
     for item in x:
         s_q.put(item)
-
-    def sort(item):
-        k, v = item
-        return float(v[type])
-    sequence = sorted(G.nodes(data=True), key=sort, reverse=True)
-    # 构建队列
-    q = Queue()
-    for k, v in sequence:
-        q.put(k)
-    flag = True
-    # 使用队列
-    rt = []
-    counter = 0
-    while flag:
-        s = s_q.get()
-        if not s_q.qsize():
-            break
-        while True:
+    init_feature(G, type)  # 第一次初始化属性
+    counter = 0  # 删除计数器
+    rt = []  # 存储返回值
+    while len(G.nodes()):
+        # 根据属性排序，构造队列q
+        def sort(item):
+            k, v = item
+            return float(v[type])
+        sequence = sorted(G.nodes(data=True), key=sort, reverse=True)
+        # 构建队列
+        q = Queue()
+        for k, v in sequence:
+            q.put(k)
+        # 使用队列 q，每次取最前边的几个
+        s = s_q.get()  # 获取本次要删除到的总数
+        while True:  # 按顺序删除节点，直到删除数达到s
             # 每次删除一批节点
             if not q.empty():
                 node = q.get()
                 G.remove_node(node)
                 counter += 1
-                if counter == s:
+                if counter >= s:
                     break
             else:
-                flag = False
                 break
         # 计算删除后状态
-        temp = {label: round(s / node_num, 2),'S': s}
+        temp = {label: round(s / node_num, 2), 'S': s}
         temp.update(caculate(G))
+        init_feature(G, type)  # 重新计算属性
         rt.append(temp)
     return rt
 
 
 def caculate(G):
+    """计算图的剩余属性"""
     asf = list(nx.connected_components(G))
-    last_num = len(G.nodes())
     wgs = len(asf)
     if asf:
         largest = max(asf, key=len)
@@ -108,10 +115,10 @@ def caculate(G):
 
 
 types = {
-    'degree':'D',
-    'eigencentrality':'E',
-    'closnesscentrality':'C',
-    'betweenesscentrality':'B'
+    'degree': 'D',
+    # 'eigencentrality': 'E',
+    # 'closnesscentrality': 'C',
+    # 'betweenesscentrality': 'B'
 }
 
 
@@ -124,7 +131,7 @@ def main():
             df = pd.DataFrame(rt)
             df.reset_index(drop=True)
             print(df)
-            path = join(dst,'{}-{}'.format(type,file))
+            path = join(dst, '{}-{}'.format(type, file))
             df.to_csv(path)
 
 
